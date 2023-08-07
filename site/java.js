6969a1ce17024e7f3507eed4c025229d6db77916
rafaelchef01 <133984307+rafaelchef01@users.noreply.github.com>
2023-08-07 08:05:15 -0300

    Delete site directory

--- site/java.js
+++
@@ -1,8 +0,0 @@
-<script type="text/javascript">
-    document.write("</script>listaNomes.html"</script>)
-    <br> 
-    <br>Corinthians<br>
-    </br>Fluminense<br>
-    </br>Atletico Paranaense<br>
-    </br>Palmeiras"
-    </script>
